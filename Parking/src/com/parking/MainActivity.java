369bb914eff60cb7b7b5f7abebcfcf39c1425091
mumuchang <14301132@bjtu.edu.cn>
2017-03-24 10:10:22 +0800

    project
    parking项目工程

---
+++ Parking/src/com/parking/MainActivity.java
@@ -0,0 +1,37 @@
+package com.parking;
+
+import cn.bmob.v3.Bmob;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Bmob.initialize(MainActivity.this, "19872817e4d9a4070addbbb47c9564f5"); 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
