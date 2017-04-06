75df5bcb7bb93b8f454b9218a52e0f3afe52cac7
123shiwen <1649252023@qq.com>
2017-04-07 00:29:29 +0800

    个人设置界面
    请验收

---
+++ Parking/src/com/parking/fragments/ShouyeFragment.java
@@ -0,0 +1,25 @@
+package com.parking.fragments;
+
+
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.parking.R;
+
+import android.app.Fragment;
+import android.os.Bundle;	
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class ShouyeFragment extends Fragment  {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		//引入我们的布局
+		return inflater.inflate(R.layout.tab01, container, false);
+	}
+}
