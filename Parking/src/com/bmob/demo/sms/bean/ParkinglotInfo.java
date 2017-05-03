1c37929ca69d6a066a82ba738014d988735078b2
fortunatekiss <candyjst@hotmail.com>
2017-05-03 22:51:14 +0800

    显示停车场信息

---
+++ Parking/src/com/bmob/demo/sms/bean/ParkinglotInfo.java
@@ -0,0 +1,7 @@
+package com.bmob.demo.sms.bean;
+
+/**
+ * Created by lenovo on 2017/5/1.
+ */
+public class ParkinglotInfo {
+}
---
+++ Parking/src/com/parking/service/DetailPopupWindow.java
@@ -0,0 +1,7 @@
+package com.parking.service;
+
+/**
+ * Created by lenovo on 2017/5/3.
+ */
+public class DetailPopupWindow {
+}
